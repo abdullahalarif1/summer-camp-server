f79a342ade9aef52f8758b4e2b63070d1fa7c826
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-06-07 18:26:20 +0600

    get the instructor data from mongo db

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
 // middleware
 app.use(cors())
 app.use(express.json())
@@ -26,6 +27,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const instructorCollection = client.db('summerDb').collection('instructor')
+
+
+        app.get('/instructors/', async (req, res) => {
+            const result = await instructorCollection.find().toArray()
+            res.send(result)
+
+
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
